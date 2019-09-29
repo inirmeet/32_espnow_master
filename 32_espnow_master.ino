2bf578ce1d518d4bcbfb5a2e1e35cde5bba27340
Nirmeet Sanghani lp <nirmeets@gmail.com>
2019-09-29 16:41:56 +0530

    Added I2C Display Code

--- 32_espnow_master.ino
+++ 32_espnow_master.ino
@@ -1,21 +1,36 @@
-//Libs for espnow and wifi
+ //Libs for espnow and wifi
 #include <esp_now.h>
 #include <WiFi.h>
 
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 //Channel used in the connection
 #define CHANNEL 1
+#define JOY_L_X 33
+#define JOY_L_Y 34
 
-// The sensor value
-int TL = 0;
-int BL = 0;
-int TR = 0;
-int BRT = 0;
+#define JOY_R_X 35
+#define JOY_R_Y 36
 
+uint32_t cnt = 0;
+
+//int JLX =0 ,JLY = 0, JRX = 0, JRY = 0;
 
 //Gpios that we are going to read (analogRead) and send to the Slaves
 //It's important that the Slave source code has this same array
 //with the same gpios in the same order
-uint8_t gpios[] = {TL,BL,TR,BRT};
+uint8_t gpios[] = {JOY_L_X,JOY_L_Y,JOY_R_X,JOY_R_Y};
 
 //In the setup function we'll calculate the gpio count and put in this variable,
 //so we don't need to change this variable everytime we change
@@ -40,6 +55,16 @@ void setup() {
 
   Serial.begin(115200);
 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+  }
+  
+   display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
   //Calculation of gpio array size:
   //sizeof(gpios) returns how many bytes "gpios" array points to.
   //Elements in this array are of type uint8_t.
@@ -56,6 +81,8 @@ void setup() {
   Serial.print("Mac Address in Station: ");
   Serial.println(WiFi.macAddress());
 
+  displayText("Mac Address:" +String(WiFi.macAddress()));
+
   //Calls the function that will initialize the ESP-NOW protocol
   InitESPNow();
 
@@ -66,7 +93,7 @@ void setup() {
   //then the total of slaves is the division of the total amount of bytes
   //by how many elements each MAc Address has
   //by how much bytes each element has.
-  int slavesCount = sizeof(macSlaves)/6/sizeof(uint8_t);
+  int slavesCount = sizeof(macSlaves)/6/sizeof(uint8_t); 
 
   //For each Slave
   for(int i=0; i<slavesCount; i++){
@@ -88,16 +115,17 @@ void setup() {
 
   //For each gpio
   //For each GPIO pin in array
-  for(int i=0; i<gpioCount; i++){
-    //We put in read mode
-    pinMode(gpios[i], INPUT);
-  }
+//  for(int i=0; i<gpioCount; i++){
+//    //We put in read mode
+//    pinMode(gpios[i], INPUT);
+//  }
 }
 
 void InitESPNow() {
   //If the initialization was successful
   if (esp_now_init() == ESP_OK) {
     Serial.println("ESPNow Init Success");
+    
   }
   //If there was an error
   else {
@@ -110,17 +138,17 @@ void InitESPNow() {
 //the read values to the others ESPs
 void send(){
   //Array that will store the read values
-  uint8_t values[gpioCount];
+  uint32_t values[gpioCount];
 
   //For each gpio
   for(int i=0; i<gpioCount; i++){
-
     //Reads the value (HIGH or LOW) of the gpio
     //and stores the value on the array
-    // values[i] = analogRead(gpios[i]);
-    values[i] = random(1, 100);
+      values[i] = analogRead(gpios[i]);
+    // values[i] = random(1, 100);
+    Serial.println(String(i)+" : "+values[i]);
   }
-
+  Serial.println("**************************************************");
   //In this example we are going to use the broadcast address {0xFF, 0xFF,0xFF,0xFF,0xFF,0xFF}
   //to send the values to all Slaves.
   //If you want to send to a specific Slave, you have to put its Mac Address on macAddr.
@@ -163,4 +191,17 @@ void loop() {
   //Sends again
   send();
   delay(1000);
+  cnt++;
+  displayText("Sent: " + String(cnt));
  }
+
+
+ void displayText(String text) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(text);
+  display.display();
+}
